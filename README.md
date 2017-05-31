766161263d45fe51fb76952345682cc5c20cac44
9crk <admin@9crk.com>
2017-05-31 19:29:15 +0800

    update

---
+++ README.md
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get -y install git
+git clone https://github.com/9crk/webrtc_study.git
+cd webrtc_study
+sudo sh install.sh
+sudo nohup sh run.sh
+
+
---
+++ install.sh
@@ -0,0 +1,27 @@
+sudo apt-get -y install nodejs
+
+git clone https://github.com/EricssonResearch/openwebrtc-examples
+
+rm openwebrtc-examples/web/client/sdp.js
+
+wget https://raw.githubusercontent.com/EricssonResearch/openwebrtc/master/bridge/client/sdp.js -O openwebrtc-examples/web/client/sdp.js
+
+openssl req -x509 -newkey rsa:2048 -keyout ~/webrtc_server_pkey.pem -out ~/webrtc_server_cert.pem -days 99999 -nodes
+
+cd openwebrtc-examples/web/
+
+nohup node channel_server.js 8080 8443 ~/webrtc_server_pkey.pem ~/webrtc_server_cert.perm &
+
+
+
+sudo apt-get -y install turnserver
+
+
+
+
+
+
+
+
+
+
