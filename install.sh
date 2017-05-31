62373415908dc0c2c3d59881468d8eef59693701
9crk <admin@9crk.com>
2017-05-31 19:57:43 +0800

    update

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+if [ "$1" == "" ]; then
+    echo "input your ip"
+    exit
+fi
 sudo apt-get -y install nodejs
 
 git clone https://github.com/EricssonResearch/openwebrtc-examples
@@ -8,8 +12,8 @@ wget https://raw.githubusercontent.com/EricssonResearch/openwebrtc/master/bridge
 
 openssl req -x509 -newkey rsa:2048 -keyout ~/webrtc_server_pkey.pem -out ~/webrtc_server_cert.pem -days 99999 -nodes
 
-sed 's/mmt-stun.verkstad.net/119.23.53.241/g' -i openwebrtc-examples/web/client/main.js
-sed 's/mmt-turn.verkstad.net/119.23.53.241/g' -i openwebrtc-examples/web/client/main.js
+sed 's/mmt-stun.verkstad.net/'$1'/g' -i openwebrtc-examples/web/client/main.js
+sed 's/mmt-turn.verkstad.net/'$1'/g' -i openwebrtc-examples/web/client/main.js
 
 #cd openwebrtc-examples/web/ 
 #nohup nodejs channel_server.js 8080 8443 ~/webrtc_server_pkey.pem ~/webrtc_server_cert.perm &
